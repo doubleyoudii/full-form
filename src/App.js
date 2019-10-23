e438aa17a148bb489580a72ec6846ad6172b135c
unknown <willyumburjer@gmail.com>
2019-10-23 11:11:41 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+import FormList from './components/FormList';
+import CreateForm from './components/CreateForm';
+import EditForm from './components/EditForm';
+import Navbar from './components/Navbar';
+import FormView from './components/FormView';
+
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className="container">
+        <Navbar/>
+        <br/>
+        <Route path="/" exact component={FormList}/>
+        <Route path="/edit/:id" component={EditForm}/>
+        <Route path="/create" component={CreateForm}/>
+        <Route path="/:id" component={FormView}/>
+      </div>
+    </Router>
+  )
 }
 
 export default App;
